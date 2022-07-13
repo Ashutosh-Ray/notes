1b1d76bb7a6efdff4b03e81f771e39f17ffa6f1e
Ashutosh Ray <manoj.ray.puri@gmail.com>
2022-07-13 10:20:33 +0530

    Please, sonali is my lucky charm

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+//import {render} from 'react-dom';
+import './index.css';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
